78392bf3439ebbdedb16f5bb6c81c1d2ba34d1b9
Dannypaul247 <danielpekpo22@gmail.com>
2024-12-28 16:48:43 +0100

    Normalize line endings

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    <h2>Here's the about</h2>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+Contact Page
+    </x-slot:heading>
+    <h2>
+        Contact Us Today
+    </h2>
+</x-layout>
